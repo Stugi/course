f9fac516912a64cc5bc39c0e96a4a5b53a300e41
arishastugi <arisha.stugi@gmail.com>
2024-08-06 23:20:11 +0300

    hw17.3.3 sync cond

--- Multithreading/17.3.atomic/cond/main.go
+++ Multithreading/17.3.atomic/cond/main.go
@@ -3,30 +3,49 @@ package main
 import (
 	"fmt"
 	"sync"
-	"sync/atomic"
+	"time"
 )
+
+//написать код многопоточной программы,
+// в которой 8 горутин параллельно осуществляют наращивание целочисленной переменной
+//с определённым шагом до какого-то предела
 
 // Перепишите приведённый выше пример со счётчиком из основного текста,
 // но вместо примитивов из пакета atomic используйте условную переменную
 // и попробуйте реализовать динамическую проверку достижения конечного значения счётчиком.
 
-const step int64 = 1
+const step int = 1
+const countGorutins int = 8
 const interationAmount int = 1000
 
 func main() {
-	var counter int64 = 0
-	var c = sync.NewCond(&sync.Mutex{})
-	increment := func(i int) {
-		atomic.AddInt64(&counter, step)
-		if i == interationAmount {
-			c.Signal()
-		}
+	start := time.Now()
+	var wg sync.WaitGroup
+	mu := sync.Mutex{}
+	c := sync.NewCond(&mu)
+
+	var counter int = 0
+
+	for i := 1; i <= countGorutins; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for {
+				c.L.Lock()
+
+				if counter >= interationAmount {
+					c.L.Unlock()
+					return
+				}
+
+				counter += step
+				fmt.Println("counter", counter, i)
+				c.L.Unlock()
+			}
+		}()
 	}
-	for i := 1; i <= interationAmount; i++ {
-		go increment(i)
-	}
-	c.L.Lock()
-	c.Wait()
-	c.L.Unlock()
+
+	wg.Wait()
 	fmt.Println(counter)
+	fmt.Println(time.Now().Sub(start).Seconds())
 }
