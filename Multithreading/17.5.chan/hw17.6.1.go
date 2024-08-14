18eb6e4816ef660e942f5f823fe7cbf7204b55b4
arishastugi <arisha.stugi@gmail.com>
2024-08-14 22:57:13 +0300

    fix hw17.6.1

--- Multithreading/17.5.chan/hw17.6.1.go
+++ Multithreading/17.5.chan/hw17.6.1.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
 //  1/3Задание 17.6.1 (HW-04)
 // Напишите код, в котором имеются два канала сообщений из целых чисел, так,
 // чтобы приём сообщений из них никогда не приводил к блокировке и
@@ -7,23 +13,46 @@ package main
 // *Если хотите, можете написать код, который бы демонстрировал это соотношение.
 
 func main() {
-	chan1 := make(chan int)
-	chan2 := make(chan int)
+	rand.Seed(time.Now().Unix())
+
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+
 	go func() {
 		for {
-			chan1 <- 1
+			sendMessage(ch1, 1)
+			time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
 		}
 	}()
+
 	go func() {
 		for {
-			chan2 <- 2
+			sendMessage(ch2, 2)
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
 		}
 	}()
-	go func() {
-		for {
-			<-chan1
-			<-chan1
-			<-chan2
+
+	count1 := 0
+	count2 := 0
+	for i := 0; i < 100; i++ {
+		select {
+		case message := <-ch1:
+			fmt.Println("Получено сообщение из канала 1:", message)
+			count1++
+		case message := <-ch2:
+			fmt.Println("Получено сообщение из канала 2:", message)
+			count2++
+		default:
+			// fmt.Println("Нет сообщений в каналах")
+			time.Sleep(10 * time.Millisecond)
 		}
-	}()
+	}
+
+	fmt.Println("Количество сообщений в канале 1:", count1)
+	fmt.Println("Количество сообщений в канале 2:", count2)
+
+}
+
+func sendMessage(ch chan int, message int) {
+	ch <- message
 }
