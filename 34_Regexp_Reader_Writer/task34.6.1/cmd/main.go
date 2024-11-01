89816e4d2aa8ef28a1c84e88fbe8f67c2cde3b9e
arishastugi <arisha.stugi@gmail.com>
2024-11-02 01:09:49 +0300

    task34.6.1 fix

--- 34_Regexp_Reader_Writer/task34.6.1/cmd/main.go
+++ 34_Regexp_Reader_Writer/task34.6.1/cmd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	rf "task34.6.1/pkg/reader"
 	mathexample "task34.6.1/pkg/types"
@@ -9,28 +12,28 @@ import (
 )
 
 func main() {
-	// reader := bufio.NewReader(os.Stdin)
-	// fmt.Print("Введите полный путь файла: ")
-	// inPath, err := reader.ReadString('\n')
-	// if err != nil {
-	// 	panic(err)
-	// }
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Введите полный путь файла: ")
+	inPath, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
 
-	// inPath = strings.TrimSpace(inPath)
-	// fmt.Println("Ваш файл: ", inPath)
+	inPath = strings.TrimSpace(inPath)
+	fmt.Println("Ваш файл: ", inPath)
 
-	// reader = bufio.NewReader(os.Stdin)
-	// fmt.Print("Введите полный путь для записи: ")
-	// outPath, err := reader.ReadString('\n')
-	// if err != nil {
-	// 	panic(err)
-	// }
+	reader = bufio.NewReader(os.Stdin)
+	fmt.Print("Введите полный путь для записи: ")
+	outPath, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
 
-	// outPath = strings.TrimSpace(outPath)
-	// fmt.Println("Ваш путь для записи: ", outPath)
+	outPath = strings.TrimSpace(outPath)
+	fmt.Println("Ваш путь для записи: ", outPath)
 
-	outPath := "./out.txt"
-	inPath := "/Users/arinastugireva/Documents/projects/learning/course/34_Regexp_Reader_Writer/task34.6.1/example.txt"
+	// outPath := "./out.txt"
+	// inPath := "/Users/arinastugireva/Documents/projects/learning/course/34_Regexp_Reader_Writer/task34.6.1/example.txt"
 
 	examples, err := rf.GetStringsFromFile(inPath)
 
